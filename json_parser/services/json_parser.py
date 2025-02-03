301a1ab20f49571341c98aad405effa667f03a67
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-02-03 22:03:05 +0200

    Enhance JSON parser to validate structure and return appropriate error messages

--- json_parser/services/json_parser.py
+++ json_parser/services/json_parser.py
@@ -2,4 +2,10 @@
 def json_parser(file):
     with open(file, "r") as f:
         data = f.read()
-    return data if data else ""
+        braces_equal = data.count("{") == data.count("}")
+        quotes_equal = data.count("\"") % 2 == 0 if data.count("\"") > 0 else True
+        commas_correct = data.count(",") == data.count(":") - 1 if data.count(",") > 0 else True
+        if data:
+            if braces_equal and quotes_equal and commas_correct:
+                return eval(data)
+    return "Invalid JSON"
--- json_parser/tests.py
+++ json_parser/tests.py
@@ -6,8 +6,24 @@ class JsonParserTestCase(TestCase):
 
     def test_json_parser_step1_invalid(self):
         file = "json_parser/tests/step1/invalid.json"
-        self.assertEqual(json_parser(file), "")
+        self.assertEqual(json_parser(file), "Invalid JSON")
 
     def test_json_parser_step1_valid(self):
         file = "json_parser/tests/step1/valid.json"
-        self.assertEqual(json_parser(file), "{}")
+        self.assertEqual(json_parser(file), {})
+
+    def test_json_parser_step2_invalid(self):
+        file = "json_parser/tests/step2/invalid.json"
+        self.assertEqual(json_parser(file), "Invalid JSON")
+
+    def test_json_parser_step2_valid(self):
+        file = "json_parser/tests/step2/valid.json"
+        self.assertEqual(json_parser(file), {"key": "value"})
+    
+    def test_json_parser_step2_invalid2(self):
+        file = "json_parser/tests/step2/invalid2.json"
+        self.assertEqual(json_parser(file), "Invalid JSON")
+
+    def test_json_parser_step2_valid2(self):
+        file = "json_parser/tests/step2/valid2.json"
+        self.assertEqual(json_parser(file), {"key": "value", "key2": "value"})
